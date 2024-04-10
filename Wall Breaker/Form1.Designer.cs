590ec28977e0ac25faa8d8ad70e74b1380aa6b78
ancablahova <ancablahova@post.cz>
2024-04-10 09:54:12 +0200

    Zaklad kodu

--- Wall Breaker/Form1.Designer.cs
+++ Wall Breaker/Form1.Designer.cs
@@ -29,12 +29,57 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pbPlatno = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tmrRedraw = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPlatno
+            // 
+            this.pbPlatno.BackColor = System.Drawing.Color.White;
+            this.pbPlatno.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbPlatno.Location = new System.Drawing.Point(12, 12);
+            this.pbPlatno.Name = "pbPlatno";
+            this.pbPlatno.Size = new System.Drawing.Size(584, 426);
+            this.pbPlatno.TabIndex = 0;
+            this.pbPlatno.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(626, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tmrRedraw
+            // 
+            this.tmrRedraw.Tick += new System.EventHandler(this.tmrRedraw_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pbPlatno);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WallBreaker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbPlatno;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer tmrRedraw;
     }
 }
 
--- Wall Breaker/Form1.cs
+++ Wall Breaker/Form1.cs
@@ -12,9 +12,57 @@ namespace Wall_Breaker
 {
     public partial class Form1 : Form
     {
+        // bitmapa do ktere kreslim
+        Bitmap mobjMyBitmap;
+        Graphics mobjPlatnoBackround;
+
+        // grafika na okne z pictureboxu
+        Graphics mobjPlatnoForm;
+
+        // souradnice kulicky
+        int mintKulickaX, mintKulickaY;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //----------------------------------------------------------------------------
+        // nahrani formu do pameti
+        //----------------------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // vytvoreni grafiky z pisctureboxu
+            mobjPlatnoForm = pbPlatno.CreateGraphics();
+
+            // vytvorenji bitmapy
+            mobjMyBitmap = new Bitmap(pbPlatno.Width,pbPlatno.Height);
+            mobjPlatnoBackround = Graphics.FromImage(mobjMyBitmap);
+
+            // nastavit kulicku
+            mintKulickaX = mintKulickaY = 10;
+
+            // nastaveni timeru prekresleni
+            tmrRedraw.Interval = 500;
+            tmrRedraw.Enabled = true;
+        }
+        
+        
+        //----------------------------------------------------------------------------
+        // prekresleni obrazu
+        //----------------------------------------------------------------------------
+        private void tmrRedraw_Tick(object sender, EventArgs e)
+        {
+            // nakresli kolecko
+            mobjPlatnoBackround.FillEllipse(Brushes.Blue, mintKulickaX, mintKulickaY, 10, 10);
+
+            // posun kulicky
+            mintKulickaX = mintKulickaX + 5;
+            mintKulickaY = mintKulickaY + 5;
+
+            // vykesleni na pbPlatno
+            mobjPlatnoForm.DrawImage(mobjMyBitmap, 0, 0);
+
         }
     }
 }
